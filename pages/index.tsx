3f4d033bf56ee99b902237b7643cc614b419f2fc
sahalshaikhh <shaikhsahal308@gmail.com>
2023-10-07 03:43:39 +0530

    Intro-web for MenuCrafter

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from "../pages/home/Home"
+import Navbar from "../components/Navbar/Navbar"
+
+function index() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  )
+}
+
+export default index
